16c8346c4f0e8ba611425b3e8834a0d0a665e917
nathanemac <91251698+nathanemac@users.noreply.github.com>
2024-03-28 11:34:34 -0400

    v0.1 of iR2-reg
    - handles nlp models
    - check for convergence conditions and updates the precisions if needed
    - optional argument verb to print info about convergence conditions updates

--- playground.jl
+++ playground.jl
@@ -1,35 +1,65 @@
 using LinearAlgebra
-using Distributions
 using NLPModels
 using ADNLPModels
 using ShiftedProximalOperators
-using RegularizedOptimization
+using RegularizedOptimization 
 using ProximalOperators
 using Printf
 using SolverCore
 using Plots
+using JSOSolvers
 
-include("MPR2_Alg.jl")
+include("iR2_Alg MP.jl")
+include("utils.jl")
 
 ####################
 
-nlp = ADNLPModel(x -> (1-x[1])^2 + 100(x[1]-x[2]^2)^2, [-1., 2.])
+nlp = ADNLPModel(x -> (1-x[1])^2 + 100(x[1]-x[2]^2)^2, [-1.55, 2.345])
 h = NormL1(1.0)
-options = ROSolverOptions(verbose=2)
+options = ROSolverOptions(verbose=1, maxIter=2000)
 
 ####################
-jso_res = R2(nlp, h, options)
-my_res = MPR2(nlp, h, options)
+jso_res = RegularizedOptimization.R2(nlp, h, options)
+my_res = MPR2(nlp, h, options, verb=true)
 
 ####################
 
 K = [k for k=1:my_res.iter]
-P = my_res.solver_specific[:p_hist]
-E = [callback_precision(P[i])[2] for i=1:my_res.iter]
+P1 = my_res.solver_specific[:p_hist]
+P2 = abs.(my_res.solver_specific[:Fhist] .- my_res.solver_specific[:Fhist][end])
+E = [callback_precision(P1[i])[2] for i=1:my_res.iter]
 
-plot(K, P, label="Niveau de précision selon les itérations", xlabel="itération", ylabel="niveau de précision")
+plot(K, P1, label="Niveau de précision selon les itérations", xlabel="itération", ylabel="niveau de précision")
 plot(K, E, label="Erreur maximale admise selon les itérations", xlabel="itération", ylabel="erreur", yscale=:log10)
+plot!(K, P2,  ylabel="", label="Distance a la solution", yscale=:log10)
 
 
-# test
+# test sur 1 probleme de OptimizationProblems
+prob = woods(n=1000)
+options = ROSolverOptions(verbose=2)
+my = MPR2(prob, h, options)
+jso = RegularizedOptimization.R2(prob, h, options)
+
+
+
+
+# Benchmark : 
+using ADNLPModels
+using SolverBenchmark
+using OptimizationProblems.ADNLPProblems
+problems = (eval(Meta.parse(problem))() for problem ∈ OptimizationProblems.meta[!, :name])
+
+h = NormL1(1.0)
+options = ROSolverOptions(verbose=0)
+
+solvers = Dict(
+  :iR2_Reg => model -> MPR2(model, h, options),
+  :R2_Reg => model -> RegularizedOptimization.R2(model, h, options),
+)
+
+stats = bmark_solvers(
+  solvers, problems,
+  skipif=prob -> (!unconstrained(prob) || get_nvar(prob) < 10),
+)
+
 
---
+++ utils.jl
@@ -0,0 +1,78 @@
+# Test conditions initiales 
+function test_κ(κs, κf, κ∇, κh, η1, η2)
+  if 1/2*κs*(1- η2) - (2κf + κ∇) ≤ 0
+    @error "Initial parameters κs, κf, κg, η2 don't respect convergence conditions."
+
+  elseif 1/2*κs*η1 - (2κf + κh) ≤ 0
+    @error "Initial parameters κs, κf, κh, η1 don't respect convergence conditions."
+  end
+end
+
+function test_condition_f(fk, sk, σk, κf, pf, ps, Π, k, verb) # p : current level of precision
+  while abs(fk[pf])*(1- 1/(1 + eps(Π[pf]))) > κf*σk*norm(sk[ps])^2
+    if ((Π[pf] == Π[end]) && (Π[ps] == Π[end]))
+      verb ==true && @warn "maximum precision already reached on f and s."
+      break
+    end
+    verb ==true && @info "condition on f not reached at iteration $k with precision $(Π[pf]) on f and $(Π[ps]) on s. Increasing precision : "
+    if Π[pf] == Π[end] 
+      verb ==true && @info " └──> maximum precision already reached on f. Trying to increase precision on s."
+      ps+=1
+    else
+      pf+=1
+    end
+    verb ==true && @info " └──> current precision on f is now $(Π[pf]) and s is now $(Π[ps])"
+  end
+  return pf, ps
+end
+
+function test_condition_h(hk, sk, σk, κh, ph, ps, Π, k, verb) # p : current level of precision
+  while abs(hk[ph])*(1- 1/(1 + eps(Π[ph]))) > κh*σk*norm(sk[ps])^2
+    ((Π[ph] == Π[end]) && (Π[ps] == Π[end])) && verb ==true && @warn "maximum precision already reached on h and s."
+    verb ==true && @info "condition on h not reached at iteration $k with precision $(Π[ph]) on h and $(Π[ps]) on s. Increasing precision : "
+    if Π[ph] == Π[end] 
+      @info " └──> maximum precision already reached on h. Trying to increase precision on s."
+      ps+=1
+    else
+      ph+=1
+    end
+    verb ==true && @info " └──> current precision on h is now $(Π[ph]) and s is now $(Π[ps])"
+  end
+  return ph, ps
+end
+
+function test_condition_∇f(gfk, sk, σk, κ∇, pg, ps, Π, k, verb) # p : current level of precision
+  while norm(gfk[pg])*(1- 1/(1 + eps(Π[pg]))) > κ∇*σk*norm(sk[ps])
+    ((Π[pg] == Π[end]) && (Π[ps] == Π[end])) && verb ==true && @warn "maximum precision already reached on ∇f and s."
+    verb ==true && @info "condition on ∇f not reached at iteration $k with precision $(Π[pg]) on ∇f and $(Π[ps]) on s. Increasing precision : "
+    if Π[ps] == Π[end] 
+      verb ==true && @info " └──> maximum precision already reached on s. Trying to increase precision on ∇f."
+      pg+=1
+    else
+      ps+=1
+    end
+    verb ==true && @info " └──> current precision on ∇f is now $(Π[pg]) and s is now $(Π[ps])"
+  end
+  return pg, ps
+end
+
+# check assumption 6
+function test_assumption_6(ξ, κs, σk, sk, Π, ps, ph, mk, hk, k, verb)
+  while ξ < 1/2*κs*σk*norm(sk[ps])^2
+    if (Π[ps] == Π[end]) && (Π[ph] == Π[end])
+      verb ==true && @warn "maximum precision already reached on h and s to satisfy Assumption 6."
+    end
+    verb ==true && @info "condition on Assumption 6 not reached at iteration $k with precision $(Π[ps]) on s and $(Π[ph]) on h. Increasing precision : "
+    if Π[ph] == Π[end]
+      verb ==true && @info " └──> maximum precision already reached on h to satisfy Assumption 6. Trying to increase precision on s."
+      ps+=1
+      mks = Π[ps](mk(sk[ps]))
+      ξ = hk[ph] - mks + max(1, abs(hk[ph])) * 10 * eps() #TODO change eps() to eps(Π[p]), but which one?
+    else
+      ph+=1
+      ξ = hk[ph] - mks + max(1, abs(hk[ph])) * 10 * eps()
+    end
+    verb ==true && @info " └──> current precision on h is now $(Π[ph]) and s is now $(Π[ps])"
+  end
+  return ps, ph, ξ
+end
